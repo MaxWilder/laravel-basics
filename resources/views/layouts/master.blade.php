4a4621f28fbd6f88a2b92a643a693e3a7e02a1c8
MaxWilder <sledge42@juno.com>
2016-04-29 14:17:37 -0700

    first commit

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>@yield('title')</title>  {{-- That's a typical blade template engine expression --}}
+
+    {{-- Links --}}
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+  </head>
+  <body>
+    @include('includes.header')
+    <div class="container">
+
+      @yield('content')
+
+    </div>
+  </body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.master')
+
+@section('title')
+  Geoff's Laravel Test
+@endsection
+
+@section('content')
+  <div class="row">
+      <div class="col-md-6">
+        <h3>Create Account</h3>
+        <form action="{{ route('signup') }}" method="post">
+          <div class="form-group">
+            <label for="email">Your E-mail</label>
+            <input class="form-control" type="text" name="email" id="email">
+          </div>
+          <div class="form-group">
+            <label for="first_name">Your First Name</label>
+            <input class="form-control" type="text" name="first_name" id="first_name">
+          </div>
+          <div class="form-group">
+            <label for="password">Your Password</label>
+            <input class="form-control" type="pasword" name="password" id="password">
+          </div>
+          <div class="form-group">
+            <label for="pasword_conf">Confirm Password</label>
+            <input class="form-control" type="pasword" name="pasword_conf" id="pasword_conf">
+          </div>
+          <button type="submit" class="btn btn-primary">Create</button>
+          <input type="hidden" name="_token" value="{{ Session::token() }}"
+        </form>
+      </div>
+      <div class="col-md-6">
+        <h3>Sign In</h3>
+        <form action="#" method="post">
+          <div class="form-group">
+            <label for="email">Your E-mail</label>
+            <input class="form-control" type="text" name="email" id="email">
+          </div>
+          <div class="form-group">
+            <label for="password">Your Password</label>
+            <input class="form-control" type="pasword" name="password" id="password">
+          </div>
+          <button type="submit" class="btn btn-primary">Sign In</button>
+        </form>
+      </div>
+  </div>
+@endsection
